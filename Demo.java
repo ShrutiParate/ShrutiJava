0ba4e50124091b42df57d41510a33537a922d2ef
ShrutiParate <123201446+ShrutiParate@users.noreply.github.com>
2024-04-11 22:15:01 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package com.studyopedia;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		int b= 8;
+		System.out.println("Sum is:" + a+b);
+		System.out.println("Sub is:" + (a-b));
+		System.out.println("Multiplication is:" + a*b);
+		System.out.println("Division is:" + (float)a/b);
+
+	}
+
+}
